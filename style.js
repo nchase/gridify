0189098366a3d369c2a5f3c21797be0576e2d47b
Noah Chase <nchase@gmail.com>
2015-07-20 14:52:13 -0400

    horizontal and vertical lines.

--- style.js
+++ style.js
@@ -13,9 +13,12 @@ module.exports = function(rootSelector){
       left: 0;
       width: 100%;
       height: 100%;
-      background-image: linear-gradient(to bottom, #0FF 0, rgba(255, 255, 255, 0) 1px);
-      background-repeat: repeat-y;
-      background-size: 100% 1rem;
+      background-image:
+        linear-gradient(to bottom, #0FF 0, rgba(255, 255, 255, 0) 1px),
+        linear-gradient(to right, #0FF 0, rgba(255, 255, 255, 0) 1px)
+      ;
+      background-repeat: repeat;
+      background-size: 1.1rem 1rem;
       background-color: transparent;
       opacity: 0.825;
       z-index: 1000;
